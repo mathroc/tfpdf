9ca9bd7632de91755431ef37a02c6838357b6e51
kazsaj <ksajkowski@gmail.com>
2017-01-24 10:17:15 +0000

    Fixing unit test (#19)

--- tests/PDFGeneratedTest.php
+++ tests/PDFGeneratedTest.php
@@ -6,28 +6,28 @@ define('FPDF_FONT_WRITE_PATH', __DIR__ . '/../build/');
 
 class PDFGeneratedTest extends TestCase
 {
-   public function testFileIsGenerated()
-   {
-      $pdfLibrary = new tFPDF\PDF();
+    public function testFileIsGenerated()
+    {
+        $pdfLibrary = new tFPDF\PDF();
 
-      $pdfLibrary->AddPage();
+        $pdfLibrary->AddPage();
 
-      $pdfLibrary->AddFont('DejaVu', '', 'DejaVuSansCondensed.ttf', true);
-      $pdfLibrary->SetFont('DejaVu', '', 14);
+        $pdfLibrary->AddFont('DejaVuSansCondensed', '', 'DejaVuSansCondensed.ttf', true);
+        $pdfLibrary->SetFont('DejaVuSansCondensed', '', 14);
 
-      $txt = file_get_contents(__DIR__ . '/test_data/HelloWorld.txt');
-      $pdfLibrary->Write(8, $txt);
+        $txt = file_get_contents(__DIR__ . '/test_data/HelloWorld.txt');
+        $pdfLibrary->Write(8, $txt);
 
-      $pdfLibrary->SetFont('Arial', '', 14);
-      $pdfLibrary->Ln(10);
-      $pdfLibrary->Write(5, "La taille de ce PDF n'est que de 12 ko.");
+        $pdfLibrary->SetFont('Arial', '', 14);
+        $pdfLibrary->Ln(10);
+        $pdfLibrary->Write(5, "La taille de ce PDF n'est que de 12 ko.");
 
-      $file = $pdfLibrary->output();
+        $file = $pdfLibrary->output();
 
-      file_put_contents(__DIR__ . '/test_data/output.pdf', $file);
+        file_put_contents(__DIR__ . '/test_data/output.pdf', $file);
 
-      if (!file_exists(__DIR__ . '/test_data/output.pdf')) {
-         static::fail();
-      }
-   }
+        if (!file_exists(__DIR__ . '/test_data/output.pdf')) {
+            static::fail();
+        }
+    }
 }
